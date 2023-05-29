ecfbb6a023144827f518f87638266494a33ff093
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-05-29 12:08:55 -0400

    Play sound effect when score is added.

--- Assets/LogicManagerScript.cs
+++ Assets/LogicManagerScript.cs
@@ -6,16 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class LogicManagerScript : MonoBehaviour
 {
-    // [SerializeField]
     public int playerScore;
     public Text textScore;
     public GameObject gameOverScreen;
+    public AudioSource dingSfx;
+
+    void Start()
+    {
+        dingSfx = GetComponent<AudioSource>();
+        dingSfx.volume = 0.5f;
+    }
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore = playerScore + scoreToAdd;
         textScore.text = playerScore.ToString();
+        dingSfx.Play(0);
     }
 
     public void restartGame()
--- Assets/PipeMiddleScript.cs
+++ Assets/PipeMiddleScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PipeMiddleScript : MonoBehaviour
 {
-    public LogicManagerScript logic;
+    private LogicManagerScript logic;
 
     // Start is called before the first frame update
     void Start()
