b8b4c45f4c87d4e782785964dec43d84eae12377
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-01 10:54:30 -0400

    Set up platform and move it between two waypoints.

---
+++ 02-platformer/Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField]
+    private float speed = 2f;
+
+    void Update()
+    {
+        if (
+            Vector2.Distance(transform.position, waypoints[currentWaypointIndex].transform.position)
+            < 0.1f
+        )
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            waypoints[currentWaypointIndex].transform.position,
+            Time.deltaTime * speed
+        );
+    }
+}
