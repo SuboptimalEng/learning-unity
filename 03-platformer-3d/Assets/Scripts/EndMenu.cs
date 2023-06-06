64e5492362102f81e1586c7501ea8d6152677907
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-06 18:16:07 -0400

    Set up start and end menus.

---
+++ 03-platformer-3d/Assets/Scripts/EndMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
