3271103060f83287d5909d8b23311818d84a9a0e
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-14 10:39:58 -0400

    Format mesh generator file.

--- 05-procedural-terrain/Assets/Scripts/MeshGenerator.cs
+++ 05-procedural-terrain/Assets/Scripts/MeshGenerator.cs
@@ -2,42 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class MeshGenerator {
-  public static void GenerateTerrainMesh(float[,] heightMap) {
-    int width = heightMap.GetLength(0);
-    int height = heightMap.GetLength(1);
+public static class MeshGenerator
+{
+    public static void GenerateTerrainMesh(float[,] heightMap)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
 
-    MeshData meshData = new MeshData(width, height);
-    int vertexIndex = 0;
+        MeshData meshData = new MeshData(width, height);
+        int vertexIndex = 0;
 
-    for (int y = 0; y < height; y++) {
-
-
-
-      for (int x = 0; x < width; x++) {
-
-
-        meshData.vertices[vertexIndex] = new Vector3(x, heightMap[x, y], y);
-      }
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                meshData.vertices[vertexIndex] = new Vector3(x, heightMap[x, y], y);
+            }
+        }
     }
-  }
 }
 
-public class MeshData {
-  public Vector3[] vertices;
-  public int[] triangles;
+public class MeshData
+{
+    public Vector3[] vertices;
+    public int[] triangles;
 
-  int triangleIndex;
+    int triangleIndex;
 
-  public MeshData(int meshWidth, int meshHeight) {
-    vertices = new Vector3[meshWidth * meshHeight];
-    triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
-  }
+    public MeshData(int meshWidth, int meshHeight)
+    {
+        vertices = new Vector3[meshWidth * meshHeight];
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+    }
 
-  public void AddTriangle(int a, int b, int c) {
-    triangles[triangleIndex] = a;
-    triangles[triangleIndex + 1] = b;
-    triangles[triangleIndex + 2] = c;
-    triangleIndex += 3;
-  }
+    public void AddTriangle(int a, int b, int c)
+    {
+        triangles[triangleIndex] = a;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
+        triangleIndex += 3;
+    }
 }
