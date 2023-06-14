d244363659524516e5289b03ea04c2ef2480c20f
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-14 18:23:27 -0400

    Finish terrain generation episode 08 - set up map and mesh data generation in threads.

--- 05-procedural-terrain/Assets/Scripts/EndlessTerrain.cs
+++ 05-procedural-terrain/Assets/Scripts/EndlessTerrain.cs
@@ -6,6 +6,7 @@ public class EndlessTerrain : MonoBehaviour
 {
     public const float maxViewDist = 450;
     public Transform viewer;
+    public Material mapMaterial;
 
     public static Vector2 viewerPosition;
     static MapGenerator mapGenerator;
@@ -68,7 +69,7 @@ public class EndlessTerrain : MonoBehaviour
                 {
                     terrainChunkDictionary.Add(
                         viewedChunkCoord,
-                        new TerrainChunk(viewedChunkCoord, chunkSize, transform)
+                        new TerrainChunk(viewedChunkCoord, chunkSize, transform, mapMaterial)
                     );
                 }
             }
@@ -81,15 +82,23 @@ public class EndlessTerrain : MonoBehaviour
         Vector2 position;
         Bounds bounds;
 
-        public TerrainChunk(Vector2 coord, int size, Transform parent)
+        MapData mapData;
+
+        MeshRenderer meshRenderer;
+        MeshFilter meshFilter;
+
+        public TerrainChunk(Vector2 coord, int size, Transform parent, Material material)
         {
             position = coord * size;
             bounds = new Bounds(position, Vector2.one * size);
             Vector3 positionV3 = new Vector3(position.x, 0, position.y);
 
-            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject = new GameObject("Terrain Chunk");
+            meshRenderer = meshObject.AddComponent<MeshRenderer>();
+            meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshRenderer.material = material;
+
             meshObject.transform.position = positionV3;
-            meshObject.transform.localScale = Vector3.one * size / 10f;
             meshObject.transform.parent = parent;
             SetVisible(false);
 
@@ -98,7 +107,12 @@ public class EndlessTerrain : MonoBehaviour
 
         void OnMapDataReceived(MapData mapData)
         {
-            print("map data received");
+            mapGenerator.RequestMeshData(mapData, OnMeshDataReceived);
+        }
+
+        void OnMeshDataReceived(MeshData meshData)
+        {
+            meshFilter.mesh = meshData.CreateMesh();
         }
 
         public void UpdateTerrainChunk()
--- 05-procedural-terrain/Assets/Scripts/MapGenerator.cs
+++ 05-procedural-terrain/Assets/Scripts/MapGenerator.cs
@@ -35,7 +35,7 @@ public class MapGenerator : MonoBehaviour
     [Range(1, 100)]
     public int seed;
 
-    [Range(1, 200)]
+    [Range(1, 50)]
     public float meshHeightMultiplier;
     public AnimationCurve meshHeightCurve;
 
@@ -46,6 +46,7 @@ public class MapGenerator : MonoBehaviour
     public TerrainType[] regions;
 
     Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
+    Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
 
     public void DrawMapInEditor()
     {
@@ -95,6 +96,30 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
+    public void RequestMeshData(MapData mapData, Action<MeshData> callback)
+    {
+        ThreadStart threadStart = delegate
+        {
+            MeshDataThread(mapData, callback);
+        };
+
+        new Thread(threadStart).Start();
+    }
+
+    void MeshDataThread(MapData mapData, Action<MeshData> callback)
+    {
+        MeshData meshData = MeshGenerator.GenerateTerrainMesh(
+            mapData.heightMap,
+            meshHeightMultiplier,
+            meshHeightCurve,
+            levelOfDetail
+        );
+        lock (meshDataThreadInfoQueue)
+        {
+            meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
+        }
+    }
+
     void Update()
     {
         if (mapDataThreadInfoQueue.Count > 0)
@@ -102,6 +127,15 @@ public class MapGenerator : MonoBehaviour
             for (int i = 0; i < mapDataThreadInfoQueue.Count; i++)
             {
                 MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue();
+                threadInfo.callback(threadInfo.parameter);
+            }
+        }
+
+        if (meshDataThreadInfoQueue.Count > 0)
+        {
+            for (int i = 0; i < meshDataThreadInfoQueue.Count; i++)
+            {
+                MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
                 threadInfo.callback(threadInfo.parameter);
             }
         }
