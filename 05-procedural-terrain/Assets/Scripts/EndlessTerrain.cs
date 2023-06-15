6832919f599dd9cb2c8634485112be85a70503ee
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-15 18:31:54 -0400

    Begin terrain generation episode 13 - set up collisions on mesh and add player controller.

--- 05-procedural-terrain/Assets/Scripts/EndlessTerrain.cs
+++ 05-procedural-terrain/Assets/Scripts/EndlessTerrain.cs
@@ -109,6 +109,7 @@ public class EndlessTerrain : MonoBehaviour
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
+        MeshCollider meshCollider;
 
         LODInfo[] detailLevels;
         LODMesh[] lodMeshes;
@@ -134,6 +135,7 @@ public class EndlessTerrain : MonoBehaviour
             meshObject = new GameObject("Terrain Chunk");
             meshRenderer = meshObject.AddComponent<MeshRenderer>();
             meshFilter = meshObject.AddComponent<MeshFilter>();
+            meshCollider = meshObject.AddComponent<MeshCollider>();
             meshRenderer.material = material;
 
             meshObject.transform.position = positionV3 * scale;
@@ -200,6 +202,7 @@ public class EndlessTerrain : MonoBehaviour
                         {
                             previousLODIndex = lodIndex;
                             meshFilter.mesh = lodMesh.mesh;
+                            meshCollider.sharedMesh = lodMesh.mesh;
                         }
                         else if (!lodMesh.hasRequestedMesh)
                         {
