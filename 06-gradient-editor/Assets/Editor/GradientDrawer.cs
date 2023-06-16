b9886c127c8c2b58755c222bd23caa656a0ce9ce
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-16 14:42:32 -0400

    Finish gradient editor episode 01 - set up a script that opens an editor when user clicks on gradient texture.

--- 06-gradient-editor/Assets/Editor/GradientDrawer.cs
+++ 06-gradient-editor/Assets/Editor/GradientDrawer.cs
@@ -6,11 +6,42 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(CustomGradient))]
 public class GradientDrawer : PropertyDrawer
 {
+    // public override float GetPropertyHeight(SerializedProperty propery, GUIContent label)
+    // {
+    //     return 30;
+    // }
+
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
+        Event guiEvent = Event.current;
         CustomGradient gradient = (CustomGradient)
             fieldInfo.GetValue(property.serializedObject.targetObject);
-        GUI.DrawTexture(position, gradient.GetTexture((int)position.width));
-        GUI.Label(position, label.text);
+        float labelWidth = GUI.skin.label.CalcSize(label).x + 5;
+        Rect textureRect = new Rect(
+            position.x + labelWidth,
+            position.y,
+            position.width - labelWidth,
+            position.height
+        );
+
+        if (guiEvent.type == EventType.Repaint)
+        {
+            GUI.Label(position, label.text);
+            GUIStyle gradientStyle = new GUIStyle();
+            gradientStyle.normal.background = gradient.GetTexture((int)position.width);
+            GUI.Label(textureRect, GUIContent.none, gradientStyle);
+        }
+        else
+        {
+            // left mouse click
+            if (guiEvent.type == EventType.MouseDown & guiEvent.button == 0)
+            {
+                if (textureRect.Contains(guiEvent.mousePosition))
+                {
+                    // open editor window
+                    EditorWindow.GetWindow<GradientEditor>();
+                }
+            }
+        }
     }
 }
