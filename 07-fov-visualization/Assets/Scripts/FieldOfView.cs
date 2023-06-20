3d3767a4b685c7f6ec605f096d7e03c341f7594b
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-20 18:05:40 -0400

    FOV Visualization Ep 02 Part 2 - Fix edge cases when view angle is wide using binary search.

--- 07-fov-visualization/Assets/Scripts/FieldOfView.cs
+++ 07-fov-visualization/Assets/Scripts/FieldOfView.cs
@@ -16,6 +16,8 @@ public class FieldOfView : MonoBehaviour
     public List<Transform> visibleTargets = new List<Transform>();
 
     public float meshResolution;
+    public int edgeResolveIterations;
+    public float edgeDistThreshold;
 
     public MeshFilter viewMeshFilter;
     Mesh viewMesh;
@@ -89,6 +91,7 @@ public class FieldOfView : MonoBehaviour
         float stepAngleSize = viewAngle / stepCount;
 
         List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo oldViewCast = new ViewCastInfo();
         for (int i = 0; i <= stepCount; i++)
         {
             float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
@@ -98,7 +101,30 @@ public class FieldOfView : MonoBehaviour
             //     Color.red
             // );
             ViewCastInfo newViewCast = ViewCast(angle);
+
+            if (i > 0)
+            {
+                bool edgeDistThresholdExceeded =
+                    Mathf.Abs(oldViewCast.dist - newViewCast.dist) > edgeDistThreshold;
+                if (
+                    oldViewCast.hit != newViewCast.hit
+                    || (oldViewCast.hit && newViewCast.hit && edgeDistThresholdExceeded)
+                )
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
             viewPoints.Add(newViewCast.point);
+            oldViewCast = newViewCast;
         }
 
         int vertexCount = viewPoints.Count + 1;
@@ -161,6 +187,36 @@ public class FieldOfView : MonoBehaviour
         );
     }
 
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDistThresholdExceeded =
+                Mathf.Abs(minViewCast.dist - newViewCast.dist) > edgeDistThreshold;
+
+            if (newViewCast.hit == minViewCast.hit && !edgeDistThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
     public struct ViewCastInfo
     {
         public bool hit;
@@ -174,6 +230,18 @@ public class FieldOfView : MonoBehaviour
             this.point = point;
             this.dist = dist;
             this.angle = angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 pointA, Vector3 pointB)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
         }
     }
 }
