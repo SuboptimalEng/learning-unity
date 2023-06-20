72a5a3c47e15e6a0a884674d959e07a628b3932b
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-06-20 17:35:10 -0400

    FOV Visualization Ep 02 Part 1 - Create a mesh that colors the users field of view to help visualize it.

--- 07-fov-visualization/Assets/Scripts/FieldOfView.cs
+++ 07-fov-visualization/Assets/Scripts/FieldOfView.cs
@@ -15,8 +15,16 @@ public class FieldOfView : MonoBehaviour
     [HideInInspector]
     public List<Transform> visibleTargets = new List<Transform>();
 
+    public float meshResolution;
+
+    public MeshFilter viewMeshFilter;
+    Mesh viewMesh;
+
     void Start()
     {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
         StartCoroutine("FindTargetsWithDelay", 0.2f);
     }
 
@@ -27,6 +35,17 @@ public class FieldOfView : MonoBehaviour
             yield return new WaitForSeconds(delay);
             FindVisibleTargets();
         }
+    }
+
+    // void Update()
+    // {
+    //     DrawFieldOfView();
+    // }
+
+    // Call this after controller has finished updating to fix jittering
+    void LateUpdate()
+    {
+        DrawFieldOfView();
     }
 
     void FindVisibleTargets()
@@ -63,6 +82,72 @@ public class FieldOfView : MonoBehaviour
         }
     }
 
+    void DrawFieldOfView()
+    {
+        // ray count
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+
+        List<Vector3> viewPoints = new List<Vector3>();
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            // Debug.DrawLine(
+            //     transform.position,
+            //     transform.position + DirectionFromAngle(angle, true) * viewRadius,
+            //     Color.red
+            // );
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoints.Add(newViewCast.point);
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        // view mesh will be child of character object
+        // all positions of vertices should be in local space relative to character
+        vertices[0] = Vector3.zero; // instead of transform.position
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            // vertices[i + 1] = viewPoints[i];
+            // convert these to local space points
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 direction = DirectionFromAngle(globalAngle, true);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, direction, out hit, viewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(
+                false,
+                transform.position + direction * viewRadius,
+                viewRadius,
+                globalAngle
+            );
+        }
+    }
+
     public Vector3 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
         if (!angleIsGlobal)
@@ -74,5 +159,21 @@ public class FieldOfView : MonoBehaviour
             0,
             Mathf.Cos(angleInDegrees * Mathf.Deg2Rad)
         );
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dist;
+        public float angle;
+
+        public ViewCastInfo(bool hit, Vector3 point, float dist, float angle)
+        {
+            this.hit = hit;
+            this.point = point;
+            this.dist = dist;
+            this.angle = angle;
+        }
     }
 }
