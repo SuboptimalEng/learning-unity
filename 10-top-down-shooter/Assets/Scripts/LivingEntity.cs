afc772fa90850707f7630b120026cb48579d0ddb
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-07-05 16:59:40 -0400

    Top down shooter episode 05 - add system to spawn enemies in waves.

--- 10-top-down-shooter/Assets/Scripts/LivingEntity.cs
+++ 10-top-down-shooter/Assets/Scripts/LivingEntity.cs
@@ -8,6 +8,8 @@ public class LivingEntity : MonoBehaviour, IDamageable
     protected float health;
     protected bool dead;
 
+    public event System.Action OnDeath;
+
     protected virtual void Start()
     {
         health = startingHealth;
@@ -26,6 +28,10 @@ public class LivingEntity : MonoBehaviour, IDamageable
     protected void Die()
     {
         dead = true;
+        if (OnDeath != null)
+        {
+            OnDeath();
+        }
         GameObject.Destroy(gameObject);
     }
 }
