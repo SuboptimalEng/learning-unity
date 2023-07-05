8ed1599631a338588445d45fa7cc440312073817
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-07-05 17:36:45 -0400

    Top down shooter episode 06 - make enemy target position a few units away from player and add a smooth attack animation.

--- 10-top-down-shooter/Assets/Scripts/Enemy.cs
+++ 10-top-down-shooter/Assets/Scripts/Enemy.cs
@@ -6,19 +6,88 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State
+    {
+        Idle,
+        Chasing,
+        Attacking
+    }
+
+    State currentState;
+
     NavMeshAgent pathfinder;
     Transform target;
+    Material skinMaterial;
+
+    Color originalColor;
+
+    float attackDistanceTheshold = .5f;
+    float timeBetweenAttacks = 1;
+
+    float nextAttactTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
 
     protected override void Start()
     {
         base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColor = skinMaterial.color;
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
 
         StartCoroutine(UpdatePath());
     }
 
-    void Update() { }
+    void Update()
+    {
+        if (Time.time > nextAttactTime)
+        {
+            float sqrDistToTarget = (target.position - transform.position).sqrMagnitude;
+
+            if (
+                sqrDistToTarget
+                < Mathf.Pow(attackDistanceTheshold + myCollisionRadius + targetCollisionRadius, 2)
+            )
+            {
+                nextAttactTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 directionToTarget = (target.position - transform.position).normalized;
+        Vector3 attackPosition = target.position - directionToTarget * (myCollisionRadius);
+
+        float attackSpeed = 3;
+        float percent = 0;
+
+        skinMaterial.color = Color.red;
+
+        while (percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        skinMaterial.color = originalColor;
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
+    }
 
     IEnumerator UpdatePath()
     {
@@ -26,10 +95,18 @@ public class Enemy : LivingEntity
 
         while (target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
-            if (!dead)
+            if (currentState == State.Chasing)
             {
-                pathfinder.SetDestination(targetPosition);
+                Vector3 directionToTarget = (target.position - transform.position).normalized;
+                // Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+                Vector3 targetPosition =
+                    target.position
+                    - directionToTarget
+                        * (myCollisionRadius + targetCollisionRadius + attackDistanceTheshold / 2);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPosition);
+                }
             }
             yield return new WaitForSeconds(refreshRate);
         }
