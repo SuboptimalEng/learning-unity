e7719fe86c1a6740a900ac3e54e8c2ea54fc1244
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-07-05 12:04:21 -0400

    Top down shooter episode 01 - add player and player controller scripts.

---
+++ 10-top-down-shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// adds RigidBody component to game object
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+        myRigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        // make the player look at a point without changing their angle
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidBody.MovePosition(myRigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+}
