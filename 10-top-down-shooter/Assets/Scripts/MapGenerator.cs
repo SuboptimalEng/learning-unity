0d4edcd69429b486e1667a6d76e757448c655a31
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-07-06 15:44:12 -0400

    Top down shooter episode 11 - block of navmesh at the edge of the map so AI does not navigate outside the map.

--- 10-top-down-shooter/Assets/Scripts/MapGenerator.cs
+++ 10-top-down-shooter/Assets/Scripts/MapGenerator.cs
@@ -7,6 +7,7 @@ public class MapGenerator : MonoBehaviour
     public Transform tilePrefab;
     public Transform obstaclePrefab;
     public Transform navmeshFloor;
+    public Transform navmeshMaskPrefab;
     public Vector2 mapSize;
     public Vector2 maxMapSize;
 
@@ -96,6 +97,44 @@ public class MapGenerator : MonoBehaviour
                 currentObstacleCount--;
             }
         }
+
+        Transform maskLeft =
+            Instantiate(
+                navmeshMaskPrefab,
+                Vector3.left * (mapSize.x + maxMapSize.x) / 4 * tileSize,
+                Quaternion.identity
+            ) as Transform;
+        maskLeft.parent = mapHolder;
+        maskLeft.localScale = new Vector3((maxMapSize.x - mapSize.x) / 2, 1, mapSize.y) * tileSize;
+
+        Transform maskRight =
+            Instantiate(
+                navmeshMaskPrefab,
+                Vector3.right * (mapSize.x + maxMapSize.x) / 4 * tileSize,
+                Quaternion.identity
+            ) as Transform;
+        maskRight.parent = mapHolder;
+        maskRight.localScale = new Vector3((maxMapSize.x - mapSize.x) / 2, 1, mapSize.y) * tileSize;
+
+        Transform maskTop =
+            Instantiate(
+                navmeshMaskPrefab,
+                Vector3.forward * (mapSize.y + maxMapSize.y) / 4 * tileSize,
+                Quaternion.identity
+            ) as Transform;
+        maskTop.parent = mapHolder;
+        maskTop.localScale =
+            new Vector3(maxMapSize.x, 1, (maxMapSize.y - mapSize.y) / 2) * tileSize;
+
+        Transform maskBottom =
+            Instantiate(
+                navmeshMaskPrefab,
+                Vector3.back * (mapSize.y + maxMapSize.y) / 4 * tileSize,
+                Quaternion.identity
+            ) as Transform;
+        maskBottom.parent = mapHolder;
+        maskBottom.localScale =
+            new Vector3(maxMapSize.x, 1, (maxMapSize.y - mapSize.y) / 2) * tileSize;
 
         navmeshFloor.localScale = new Vector3(maxMapSize.x, maxMapSize.y) * tileSize;
     }
