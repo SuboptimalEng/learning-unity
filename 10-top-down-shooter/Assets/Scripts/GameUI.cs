0822a439acec6dbd43430e6c9b1b0d7ea31a89e2
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-07-06 18:44:13 -0400

    Top down shooter episode 14 - create game over screen and transition to it when the player loses.

---
+++ 10-top-down-shooter/Assets/Scripts/GameUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public Image fadePlane;
+    public GameObject gameOverUI;
+
+    void Start()
+    {
+        FindObjectOfType<Player>().OnDeath += OnGameOver;
+    }
+
+    void OnGameOver()
+    {
+        StartCoroutine(Fade(Color.clear, Color.black, 1));
+        gameOverUI.SetActive(true);
+    }
+
+    IEnumerator Fade(Color from, Color to, float time)
+    {
+        float speed = 1 / time;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * speed;
+            fadePlane.color = Color.Lerp(from, to, percent);
+            yield return null;
+        }
+    }
+}
--- 10-top-down-shooter/Assets/Scripts/LivingEntity.cs
+++ 10-top-down-shooter/Assets/Scripts/LivingEntity.cs
@@ -31,6 +31,7 @@ public class LivingEntity : MonoBehaviour, IDamageable
         }
     }
 
+    [ContextMenu("Self Destruct")]
     protected void Die()
     {
         dead = true;
