2ab344e1fabba30e8640fa01c51af72d90ecdd42
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-07-07 18:55:48 -0400

    Top down shooter episode 22 - add a game UI that shows the wave and number of enemies.

--- 10-top-down-shooter/Assets/Scripts/GameUI.cs
+++ 10-top-down-shooter/Assets/Scripts/GameUI.cs
@@ -9,9 +9,63 @@ public class GameUI : MonoBehaviour
     public Image fadePlane;
     public GameObject gameOverUI;
 
+    public RectTransform newWaveBanner;
+
+    public Text newWaveTitle;
+    public Text newWaveEnemyCount;
+
+    Spawner spawner;
+
     void Start()
     {
         FindObjectOfType<Player>().OnDeath += OnGameOver;
+    }
+
+    void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+    void OnNewWave(int waveNumber)
+    {
+        string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+        newWaveTitle.text = "- Wave " + numbers[waveNumber - 1] + " -";
+        string enemyCount =
+            ((spawner.waves[waveNumber - 1].infinite))
+                ? "Infinite"
+                : spawner.waves[waveNumber - 1].enemyCount.ToString();
+        newWaveEnemyCount.text = "Enemies: " + enemyCount;
+
+        StopCoroutine("AnimateNewWaveBanner");
+        StartCoroutine("AnimateNewWaveBanner");
+    }
+
+    IEnumerator AnimateNewWaveBanner()
+    {
+        float delayTime = 2f;
+        float speed = 3f;
+        float animatePercent = 0;
+        int dir = 1;
+
+        float endDelayTime = Time.time + 1 / speed + delayTime;
+
+        while (animatePercent >= 0)
+        {
+            animatePercent += Time.deltaTime * speed * dir;
+
+            if (animatePercent >= 1)
+            {
+                animatePercent = 1;
+                if (Time.time > endDelayTime)
+                {
+                    dir = -1;
+                }
+            }
+
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-270, -50, animatePercent);
+            yield return null;
+        }
     }
 
     void OnGameOver()
