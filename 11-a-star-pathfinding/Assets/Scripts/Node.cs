d862c4f5038f3a1b055477a409041373e2ffa2e1
Suboptimal Engineer <suboptimaleng@gmail.com>
2023-07-21 11:54:05 -0400

    A* episode 02 - create a map with obstacles and generate a grid of nodes that can detect walkable regions by using a Physics.SphereCheck with layer mask.

---
+++ 11-a-star-pathfinding/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool walkable, Vector3 worldPosition)
+    {
+        this.walkable = walkable;
+        this.worldPosition = worldPosition;
+    }
+}
